cebac184d3d16d8609aabbd7f876d85872c9f5f0
tkolsrud <mfliltim@gmail.com>
2021-05-14 11:24:26 -0700

    templates folder created, connected to views, imported bunch of random stuff to views

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
+
+# auth imports
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 
 # Create your views here.
 
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse("Wayfarer Mom loves to be cozy")
+class Home(TemplateView):
+    template_name = "home.html"
