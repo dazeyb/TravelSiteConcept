814e4d604c623fa74785eedda73868510c794f6b
dazeyb <btdazey@gmail.com>
2021-05-15 17:43:45 -0700

    Added logout redirect in settings.py

--- project2_project/settings.py
+++ project2_project/settings.py
@@ -132,3 +132,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Adding this so logout takes us home
+LOGOUT_REDIRECT_URL = '/'
